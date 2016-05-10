fdb71d46aa5e6d71ac18d13b64528b610977b9a0
Ian Lenehan <ianlenehan@gmail.com>
2016-05-11 09:18:20 +1000

    updared seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,8 +9,8 @@ i2 = Item.create :name => "Work Desk", :description => "Pine wood, 3 years old",
 i3 = Item.create :name => "Sweater", :description => "full of moth balls, stained armpits", :price => 0
 i4 = Item.create :name => "DVDs", :description => "selection of 15 DVDs", :price => 40
 
-u1 = User.create :name_first => "Ian", :name_last => "Lenehan", :email => "ianlenehan@me.com", :password => "balmain", :admin => true
-u2 = User.create :name_first => "Templeton", :name_last => "Peck", :email => "temp@me.com", :password => "balmain"
+u1 = User.create :name_first => "Ian", :name_last => "Lenehan", :email => "ianlenehan@me.com", :admin => true
+u2 = User.create :name_first => "Templeton", :name_last => "Peck", :email => "temp@me.com"
 
 c1 = Category.create :name => "Cars"
 c2 = Category.create :name => "Furniture"
